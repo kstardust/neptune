d8c505caa29a4c8f743e934c23a0fe955b4a13b9
kevin <lql0880@175game.com>
2019-09-03 10:25:03 +0800

    replace bytestring with bytearray

--- transport/neptune.py
+++ transport/neptune.py
@@ -5,15 +5,16 @@ from logf import logger
 
 
 class Neptune(PeerAbstract):
-    ProtocolMetaSize = 8
+    ProtocolMeta = struct.Struct('!HHHH')
     ProtocolMagNum = 0x0001
 
     def __init__(self, *args, **kargs):
         super().__init__(*args, **kargs)
         self._socket = self.writer.get_extra_info('socket')
         self.peername = self._socket.getpeername()
-        self.buff = b''
+        self.buff = bytearray()
         self.clen = 0
+        self.msg_len = 0
 
     async def on_connect(self):
         logger.info("connected: {}".format(self.peername))
@@ -21,24 +22,25 @@ class Neptune(PeerAbstract):
     async def on_input(self, data):
         if data:
             self.buff += data
+            self.msg_len += len(data)
 
-        msg_len = len(self.buff)
         if self.clen == 0:
-            if msg_len < self.ProtocolMetaSize:
+            if self.msg_len < self.ProtocolMeta.size:
                 return
 
-            mag_num, self.clen = struct.unpack_from('!HH', self.buff, 0)
+            mag_num, self.clen, *_ = self.ProtocolMeta.unpack_from(self.buff, 0)
             if mag_num != self.ProtocolMagNum:
                 self.quick_close()
                 return
 
-        if msg_len >= self.clen:
+        if self.msg_len >= self.clen:
             await self.on_message(
-                self.buff[self.ProtocolMetaSize: self.clen]
+                self.buff[self.ProtocolMeta.size: self.clen]
             )
-            self.buff = self.buff[self.clen:]
-            self.clen = 0
 
+            self.buff[:self.clen] = []
+            self.msg_len -= self.clen
+            self.clen = 0
             # process remain data in buff
             await self.on_input(None)
 
@@ -47,10 +49,10 @@ class Neptune(PeerAbstract):
 
     async def send(self, data):
         # FIXME: message length limit
-        length = len(data) + self.ProtocolMetaSize
+        length = len(data) + self.ProtocolMeta.size
         reserve = 0
-        data = struct.pack(
-            '!HHHH', self.ProtocolMagNum, length, reserve, reserve
+        data = self.ProtocolMeta.pack(
+            self.ProtocolMagNum, length, reserve, reserve
         ) + data
         await super().send(data)
 

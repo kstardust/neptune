e3c43079f003919cd4d4e50a3d42fbded9d8783e
stardust <lql9705@gmail.com>
2021-02-27 22:35:07 +0800

    relocate

---
+++ neptune_py/logic/game_server/game_logic/__init__.py
@@ -0,0 +1,19 @@
+class FightBase:
+    def __init__(self):
+        pass
+
+
+class BuffBase:
+    def __init__(self):
+        pass
+
+
+class EffectBase:
+    def __init__(self):
+        pass
+
+
+class AttributeComponentBase:
+    def __init__(self):
+        pass
+
